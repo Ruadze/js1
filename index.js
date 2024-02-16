245226b6fc0e8d3a901fbc6b9171288128db2635
Ruadze <nona.ruadze.1@iliauni.edu.ge>
2024-02-16 19:38:54 +0400

    sum

---
+++ index.js
@@ -0,0 +1,13 @@
+for(var index = 5;  index >0; index--)
+{
+    console.log(index);
+}
+
+
+//
+for(var index = 0 ; index < 10 ; index ++)
+{
+    if(index % 2 !== 0) ; 
+}
+{ console.log(index);
+}
